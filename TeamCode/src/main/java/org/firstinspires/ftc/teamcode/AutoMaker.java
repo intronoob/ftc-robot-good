5ec2a19beda20268b4c99687f1355d4b3d9ef4cb
Artem <artyom.mva@gmail.com>
2026-01-27 23:24:26 -0500

    fully working auto maker

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMaker.java
@@ -2,13 +2,15 @@
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.helpers.ArduinoComms;
 
  @TeleOp(name = "AutoMaker (Debug)")
 public class AutoMaker extends OpMode {
     private ArduinoComms comm;
     private LauncherController launchSystem;
+     ElapsedTime timer;
 //    private DcMotor roll_in;
 //    private DcMotor roll_mid_right;
 //    private DcMotor launcher;
@@ -18,12 +20,16 @@ public class AutoMaker extends OpMode {
     private RobotMovementController WheelDrive;
     private double MOVPWR = 0.25;
     private double MOV_AMT_INCREASE = 1;
+    private double time = 0;
     private double moveAmt = 0.5;
     private double xMoveAmt = 0;
     private double yMoveAmt = 0;
     private double rotMoveAmt = 0;
+    private double intakeMsStart = 0;
+    private double intakeMsEnd = 0;
     @Override
     public void init() {
+        timer = new ElapsedTime();
         comm = new ArduinoComms(hardwareMap,"pin0","pin1","pin2");
         WheelDrive = new RobotMovementController(hardwareMap);
         launchSystem = new LauncherController(hardwareMap);
@@ -39,14 +45,16 @@ public class AutoMaker extends OpMode {
     public void loop() {
         // ---- RESET ----
         if (gamepad1.leftStickButtonWasReleased()) {
-            if(xMoveAmt != 0) {
-                comm.sendData(xMoveAmt<<8|0x0<<4 ,40);
-            }
+//            if(xMoveAmt != 0) {
+////                comm.sendData(xMoveAmt<<8|0x0<<4 ,40);
+//            }
             xMoveAmt = 0;
             yMoveAmt = 0;
             rotMoveAmt = 0;
-            comm.sendData(0x0F,32);
-            comm.sleepMs(1000);
+            intakeMsStart = 0;
+            intakeMsEnd = 0;
+//            comm.sendData(0x0F,32);
+//            comm.sleepMs(1000);
             moveAmt = MOV_AMT_INCREASE;
         } else if (gamepad1.dpadUpWasReleased()) {
             moveAmt += MOV_AMT_INCREASE;
@@ -72,6 +80,15 @@ public class AutoMaker extends OpMode {
             rotMoveAmt -= moveAmt;
         } else if (gamepad1.rightStickButtonWasReleased()) {
             launchSystem.runLaunchSequence();
+        } else if (gamepad1.rightTriggerWasReleased()) {
+            time = timer.milliseconds();
+            launchSystem.toggleIntake();
+            if(launchSystem.intakeState) {
+                intakeMsStart = time;
+            } else {
+                intakeMsEnd = time;
+            }
+//            sleep(2000);
         }
 
         // ---- TELEMETRY ----
@@ -79,6 +96,7 @@ public class AutoMaker extends OpMode {
         telemetry.addData("X Move (in)", "%.2f", xMoveAmt);
         telemetry.addData("Y Move (in)", "%.2f", yMoveAmt);
         telemetry.addData("Rot Move (in)", "%.2f", rotMoveAmt);
+        telemetry.addData("time of intake (ms)", "%f", intakeMsEnd-intakeMsStart);
         telemetry.update();
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LauncherController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LauncherController.java
@@ -9,13 +9,15 @@ public class LauncherController {
 
     private static final double LAUNCH_POWER_INIT = 0.52;
     private static final double LAUNCH_VOLTAGE_INIT = 13.85;
+    private static final double OPEN_POSITION = 1;
+    private static final double CLOSE_POSITION = 0;
 
     private final DcMotor launch;
     private final DcMotor rollIn;
     private final DcMotor rollMidRight;
     private final Servo trapDoor;
     private final HardwareMap hardwareMap;
-
+    public boolean intakeState = false;
     public LauncherController(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
 
@@ -38,7 +40,7 @@ public class LauncherController {
     public void runLaunchSequence() {
         // get battery voltage
         double voltage = hardwareMap.voltageSensor.iterator().next().getVoltage();
-
+        trapDoor.setPosition(OPEN_POSITION);
         // spin up launcher
         double compensatedPower = LAUNCH_POWER_INIT * (LAUNCH_VOLTAGE_INIT / voltage);
         launch.setPower(compensatedPower);
@@ -50,10 +52,17 @@ public class LauncherController {
         rollMidRight.setPower(1.0);
 
         sleep(5000);
+        trapDoor.setPosition(CLOSE_POSITION);
 
         // stop everything
         launch.setPower(0);
         rollIn.setPower(0);
         rollMidRight.setPower(0);
+    }
+
+    public void toggleIntake() {
+        intakeState = !intakeState;
+        rollIn.setPower(intakeState ? 1 : 0);
+        rollMidRight.setPower(intakeState ? 1 : 0);
     }
 }
